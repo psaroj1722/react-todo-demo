fb699b44d079e61510b0aa68e6aa4ce06c58f6e2
psaroj1722 <prashantsaroj17@gmail.com>
2019-11-13 18:29:53 +0530

    update files

--- src/component/layout/header.js
+++ src/component/layout/header.js
@@ -4,8 +4,10 @@ import {Link} from 'react-router-dom';
 function Header(){
     return(
         <header className="header">
+          
             <h1>Todo List</h1>
             <Link className="menu-list" to="/">Home</Link> | <Link className="menu-list" to="/about">About</Link>
+           
         </header>
     )
 }
